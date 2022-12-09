d7846b4c7368923d3f1ca1b908d428895673689c
KenanTurner <coolspykee@satx.rr.com>
2022-12-09 14:07:52 -0600

    Added url validation

--- php/youtube-dl.php
+++ php/youtube-dl.php
@@ -3,14 +3,14 @@
 		http_response_code($code);
 		exit($message);
 	}
-	$url = explode(",", $_GET["url"]);
+	$url = $_GET["url"];
     if(empty($url)) error("URL search parameter is required!");
-    if(filter_var($url, FILTER_VALIDATE_URL) === FALSE) {
-        error('Not a valid URL');
+    if(!filter_var($url, FILTER_VALIDATE_URL)){
+        error('Not a valid URL!');
     }
 	
 	$result;
-	$args = array_merge(["python3","./yt-dlp","-j","-f 'bestaudio'","2>/dev/null"], $url);
+	$args = ["python3","./yt-dlp","-j","-f 'bestaudio'","2>/dev/null",$url];
 	passthru(join(" ",$args),$result);
 	if($result != 0) error("Error: process exited with code ".$result);
 ?>
